131cc8fd52b6f36800addf19e54a368124e9bbf4
Simon Oroño <sron611@gmail.com>
2016-01-16 11:58:18 -0430

    Default values for dirfix.py and new license

--- dirfix.py
+++ dirfix.py
@@ -1,4 +1,17 @@
 #!/bin/env python
+# Copyright 2016 Simón Oroño
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 """
 Utility to rename files as a secuence of numbers with trailing zeros
@@ -13,8 +26,8 @@ if __name__ == '__main__':
     directory = '.dirfix'
 
     parser = argparse.ArgumentParser(description="Directory structure fixer")
-    parser.add_argument('-order', type=str, default='',
-                        help="Specify order (random, date)")
+    parser.add_argument('-order', type=str, default='date',
+                        help="Specify order (random, date [default])")
     args = parser.parse_args()
 
     file_list = [x for x in os.listdir()
@@ -30,6 +43,9 @@ if __name__ == '__main__':
             file_list.sort(key=os.path.getmtime)
 
     digits = len(str(len(file_list)))
+    if digits < 3:
+        digits = 3
+
     new_names = []
     n = 1
 
